f744d2c4f0d9435a1c73df7471e5d3f1f4362209
oneight2 <syarif.cibatu@gmail.com>
2022-01-22 21:55:30 +0700

    fix build

--- components/organism/EditProfile/index.tsx
+++ components/organism/EditProfile/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 import Cookies from "js-cookie"
 import jwtDecode from "jwt-decode"
--- components/organism/TransactionsContent/index.tsx
+++ components/organism/TransactionsContent/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useCallback, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { getLastTransactions } from "../../../services/player";
